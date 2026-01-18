ac309c55cd3383950c5132c919b2d649f3e91597
Abizer Saify <abizersaifee5253@gmail.com>
2026-01-18 15:14:08 +0530

    viteconfig

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    // Increases the warning limit to 1000 KB (1 MB)
+    chunkSizeWarningLimit: 1000, 
+  }
 })
